780dac2179fb106dccba5da176a599772481cdd9
Marc Lorenz Menguito <c17-2807-458@uphsl.edu.ph>
2020-11-11 18:22:10 +0800

    Test
    Test

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject tile;
+
+    public float TileSize
+    {
+        get
+        {
+            return tile.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+        }
+    }
+
+    private void PlaceTile(int x, int y, Vector3 worldStart)
+    {
+        GameObject newtile = Instantiate(tile);
+        newtile.transform.position = new Vector3(worldStart.x + (TileSize * x), worldStart.y - (TileSize * y), 0);
+    }
+
+
+    private void createLevel()
+    {
+        Vector3 worldStart = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height));
+
+        for (int y = 0; y < 5; y++)
+        {
+            for (int x = 0; x < 5; x++)
+            {
+                PlaceTile(x, y, worldStart);
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        createLevel(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
